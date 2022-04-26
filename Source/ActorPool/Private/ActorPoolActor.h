fd05e83ad391f8a6dd5f563f08003d24cd67fdb3
Viktor Blomme <viktor.blomme@fishingcactus.com>
2022-04-26 16:42:54 +0200

    Move code around
    Move code from the subsystem to the actor.
    Adapt the code a little bit, so the actor is not just a copy of the subsystem, but rather a helper for it.
    Add some checks.

--- Source/ActorPool/Private/ActorPoolActor.h
+++ Source/ActorPool/Private/ActorPoolActor.h
@@ -1,9 +1,42 @@
 ﻿#pragma once
 
+#include "ActorPoolSettings.h"
+
 #include <CoreMinimal.h>
 #include <GameFramework/Actor.h>
 
 #include "ActorPoolActor.generated.h"
+
+struct FActorPoolInfos;
+
+USTRUCT()
+struct FActorPoolInstances
+{
+    GENERATED_USTRUCT_BODY()
+
+public:
+    FActorPoolInstances();
+    FActorPoolInstances( UWorld * world, const FActorPoolInfos & pool_infos );
+
+    AActor * GetAvailableInstance( UWorld * world );
+    bool ReturnActor( AActor * actor );
+    void DestroyActors();
+    void DestroyUnusedInstances();
+
+#if !( UE_BUILD_SHIPPING || UE_BUILD_TEST )
+    void DumpPoolInfos( FOutputDevice & output_device ) const;
+#endif
+
+private:
+    void DisableActor( AActor * actor ) const;
+    AActor * SpawnActorAndAddToInstances( UWorld * world );
+
+    UPROPERTY()
+    TArray< AActor * > Instances;
+
+    int AvailableInstanceIndex;
+    FActorPoolInfos PoolInfos;
+};
 
 UCLASS()
 class ACTORPOOL_API AActorPoolActor : public AActor
@@ -14,4 +47,22 @@ public:
     AActorPoolActor();
 
     void BeginPlay() override;
+    void EndPlay( const EEndPlayReason::Type end_play_reason ) override;
+
+    bool IsActorClassPoolable( TSubclassOf< AActor > actor_class ) const;
+
+    AActor * GetActorFromPool( TSubclassOf< AActor > actor_class );
+
+    bool ReturnActorToPool( AActor * actor );
+
+#if !( UE_BUILD_SHIPPING || UE_BUILD_TEST )
+    void DestroyUnusedInstancesInPools();
+    void DumpPoolInfos( FOutputDevice & output_device ) const;
+#endif
+
+private:
+    FActorPoolInstances CreateActorPoolInstance( const FActorPoolInfos & pool_infos ) const;
+
+    UPROPERTY()
+    TMap< TSubclassOf< AActor >, FActorPoolInstances > ActorPools;
 };
--- Source/ActorPool/Private/ActorPoolSubSystem.cpp
+++ Source/ActorPool/Private/ActorPoolSubSystem.cpp
@@ -1,226 +1,6 @@
 #include "ActorPoolSubSystem.h"
 
-#include "APPooledActorInterface.h"
-#include "ActorPoolSettings.h"
-
-#include <Engine/Engine.h>
-#include <Engine/World.h>
-#include <GameFramework/GameModeBase.h>
-
-#if !( UE_BUILD_SHIPPING || UE_BUILD_TEST )
-static FAutoConsoleCommandWithWorld GActorPoolDestroyInstancesInPools(
-    TEXT( "ActorPool.DestroyUnusedInstancesInPools" ),
-    TEXT( "Destroys all actors in the pools which have not been acquired." ),
-    FConsoleCommandWithWorldDelegate::CreateLambda( []( const UWorld * world ) {
-        if ( auto * system = world->GetSubsystem< UActorPoolSubSystem >() )
-        {
-            system->DestroyUnusedInstancesInPools();
-        }
-    } ),
-    ECVF_Default );
-
-static FAutoConsoleCommandWithWorldArgsAndOutputDevice GActorPoolDumpPoolInfos(
-    TEXT( "ActorPool.DumpPoolInfos" ),
-    TEXT( "Dumps infos about the pools." ),
-    FConsoleCommandWithWorldArgsAndOutputDeviceDelegate::CreateLambda( []( const TArray< FString > & /*args*/, const UWorld * world, FOutputDevice & output_device ) {
-        if ( const auto * system = world->GetSubsystem< UActorPoolSubSystem >() )
-        {
-            system->DumpPoolInfos( output_device );
-        }
-    } ),
-    ECVF_Default );
-
-static TAutoConsoleVariable< int32 > GActorPoolForceInstanceCreationWhenPoolIsEmpty(
-    TEXT( "ActorPool.ForceInstanceCreationWhenPoolIsEmpty" ),
-    1,
-    TEXT( "When on, will force to create actor instances when the pool is empty.\n" )
-        TEXT( "0: Disable, 1: Enable" ),
-    ECVF_Default );
-
-static TAutoConsoleVariable< int32 > GActorPoolDisable(
-    TEXT( "ActorPool.Disable" ),
-    0,
-    TEXT( "When on, will not create any instances.\n" )
-        TEXT( "0: Enable the pools, 1: Disable the pools" ),
-    ECVF_Default );
-#endif
-
-FActorPoolInstances::FActorPoolInstances() :
-    AvailableInstanceIndex( INDEX_NONE )
-{
-}
-
-FActorPoolInstances::FActorPoolInstances( UWorld * world, const FActorPoolInfos & pool_infos ) :
-    AvailableInstanceIndex( INDEX_NONE ),
-    PoolInfos( pool_infos )
-{
-    Instances.Reserve( pool_infos.Count );
-
-    for ( auto index = 0; index < pool_infos.Count; ++index )
-    {
-        auto * actor = SpawnActorAndAddToInstances( world );
-        DisableActor( actor );
-    }
-
-    AvailableInstanceIndex = 0;
-}
-
-AActor * FActorPoolInstances::GetAvailableInstance( UWorld * world )
-{
-    if ( AvailableInstanceIndex == Instances.Num() )
-    {
-        const auto can_create_instance = PoolInfos.bAllowNewInstancesWhenPoolIsEmpty
-#if !( UE_BUILD_SHIPPING || UE_BUILD_TEST )
-                                         || GActorPoolForceInstanceCreationWhenPoolIsEmpty.GetValueOnGameThread()
-#endif
-            ;
-
-        if ( can_create_instance )
-        {
-            SpawnActorAndAddToInstances( world );
-        }
-    }
-
-    auto * result = Instances[ AvailableInstanceIndex ];
-
-    result->SetActorHiddenInGame( !PoolInfos.AcquireFromPoolSettings.bShowActor );
-    result->SetActorEnableCollision( PoolInfos.AcquireFromPoolSettings.bEnableCollision );
-
-    if ( PoolInfos.AcquireFromPoolSettings.bDisableNetDormancy )
-    {
-        result->SetNetDormancy( PoolInfos.AcquireFromPoolSettings.NetDormancy );
-    }
-
-    if ( auto * pooled_actor_interface = Cast< IAPPooledActorInterface >( result ) )
-    {
-        IAPPooledActorInterface::Execute_OnAcquiredFromPool( result );
-    }
-
-    AvailableInstanceIndex++;
-
-    return result;
-}
-
-bool FActorPoolInstances::ReturnActor( AActor * actor )
-{
-    if ( actor == nullptr )
-    {
-        return false;
-    }
-
-    const auto index = Instances.Find( actor );
-
-    if ( index == INDEX_NONE )
-    {
-        return false;
-    }
-
-    DisableActor( actor );
-
-    check( AvailableInstanceIndex >= 0 && AvailableInstanceIndex <= Instances.Num() );
-
-    if ( Instances.Num() > 1 )
-    {
-        Instances.RemoveAt( index, 1, false );
-        Instances.Insert( actor, AvailableInstanceIndex - 1 );
-    }
-
-    AvailableInstanceIndex--;
-
-    return true;
-}
-
-void FActorPoolInstances::DestroyActors()
-{
-    for ( auto * instance : Instances )
-    {
-        if ( IsValid( instance ) )
-        {
-            instance->Destroy();
-        }
-    }
-
-    Instances.Reset();
-    AvailableInstanceIndex = 0;
-}
-
-void FActorPoolInstances::DestroyUnusedInstances()
-{
-    for ( auto index = AvailableInstanceIndex; index < Instances.Num(); index++ )
-    {
-        if ( auto * instance = Instances[ index ] )
-        {
-            instance->Destroy();
-        }
-    }
-
-    Instances.SetNum( AvailableInstanceIndex );
-}
-
-#if !( UE_BUILD_SHIPPING || UE_BUILD_TEST )
-void FActorPoolInstances::DumpPoolInfos( FOutputDevice & output_device ) const
-{
-    output_device.Logf( ELogVerbosity::Verbose, TEXT( "Pool for class %s" ), *PoolInfos.ActorClass.ToString() );
-    output_device.Logf( ELogVerbosity::Verbose, TEXT( "   Total Instance Count : %i" ), PoolInfos.Count );
-    output_device.Logf( ELogVerbosity::Verbose, TEXT( "   Alive Instance Count : %i" ), AvailableInstanceIndex );
-    output_device.Logf( ELogVerbosity::Verbose, TEXT( "   Available Instance Count : %i" ), ( PoolInfos.Count - AvailableInstanceIndex ) );
-}
-#endif
-
-void FActorPoolInstances::DisableActor( AActor * actor ) const
-{
-    actor->SetActorHiddenInGame( true );
-    actor->SetActorEnableCollision( false );
-    actor->SetNetDormancy( ENetDormancy::DORM_DormantAll );
-
-    if ( auto * pooled_actor_interface = Cast< IAPPooledActorInterface >( actor ) )
-    {
-        IAPPooledActorInterface::Execute_OnReturnedToPool( actor );
-    }
-}
-
-AActor * FActorPoolInstances::SpawnActorAndAddToInstances( UWorld * world )
-{
-    FActorSpawnParameters spawn_parameters;
-    spawn_parameters.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
-
-    auto * actor = world->SpawnActor< AActor >( PoolInfos.ActorClass.LoadSynchronous(), spawn_parameters );
-    Instances.Add( actor );
-
-    return actor;
-}
-
-UActorPoolSubSystem::UActorPoolSubSystem()
-{
-}
-
-void UActorPoolSubSystem::Initialize( FSubsystemCollectionBase & collection )
-{
-    Super::Initialize( collection );
-
-    if ( auto * world = GetWorld() )
-    {
-        if ( world->IsGameWorld() )
-        {
-            world->OnWorldBeginPlay.AddUObject( this, &UActorPoolSubSystem::OnWorldBeginPlay );
-        }
-    }
-}
-
-void UActorPoolSubSystem::Deinitialize()
-{
-    for ( auto & key_pair : ActorPools )
-    {
-        key_pair.Value.DestroyActors();
-    }
-
-    if ( auto * world = GetWorld() )
-    {
-        world->OnWorldBeginPlay.RemoveAll( this );
-    }
-
-    Super::Deinitialize();
-}
+#include "ActorPoolActor.h"
 
 bool UActorPoolSubSystem::IsActorPoolable( AActor * actor ) const
 {
@@ -234,30 +14,22 @@ bool UActorPoolSubSystem::IsActorPoolable( AActor * actor ) const
 
 bool UActorPoolSubSystem::IsActorClassPoolable( const TSubclassOf< AActor > actor_class ) const
 {
-    if ( actor_class == nullptr )
+    if ( !ensureMsgf( ActorPoolActor != nullptr, TEXT( "%s - ActorPoolActor is not valid!" ), TEXT( __FUNCTION__ ) ) )
     {
         return false;
     }
 
-    for ( const auto & key_pair : ActorPools )
-    {
-        if ( actor_class == key_pair.Key )
-        {
-            return true;
-        }
-    }
-
-    return false;
+    return ActorPoolActor->IsActorClassPoolable( actor_class );
 }
 
 AActor * UActorPoolSubSystem::GetActorFromPool( const TSubclassOf< AActor > actor_class )
 {
-    if ( auto * actor_instances = ActorPools.Find( actor_class ) )
+    if ( !ensureMsgf( ActorPoolActor != nullptr, TEXT( "%s - ActorPoolActor is not valid!" ), TEXT( __FUNCTION__ ) ) )
     {
-        return actor_instances->GetAvailableInstance( GetWorld() );
+        return nullptr;
     }
 
-    return nullptr;
+    return ActorPoolActor->GetActorFromPool( actor_class );
 }
 
 AActor * UActorPoolSubSystem::GetActorFromPoolWithTransform( const TSubclassOf< AActor > actor_class, const FTransform transform )
@@ -273,58 +45,38 @@ AActor * UActorPoolSubSystem::GetActorFromPoolWithTransform( const TSubclassOf< 
 
 bool UActorPoolSubSystem::ReturnActorToPool( AActor * actor )
 {
-    if ( actor == nullptr )
+    if ( !ensureMsgf( ActorPoolActor != nullptr, TEXT( "%s - ActorPoolActor is not valid!" ), TEXT( __FUNCTION__ ) ) )
     {
         return false;
     }
 
-    if ( auto * actor_instances = ActorPools.Find( actor->GetClass() ) )
-    {
-        return actor_instances->ReturnActor( actor );
-    }
+    return ActorPoolActor->ReturnActorToPool( actor );
+}
 
-    return false;
+void UActorPoolSubSystem::RegisterActorPoolActor( AActorPoolActor * actor_pool_actor )
+{
+    ActorPoolActor = actor_pool_actor;
+    ensureAlwaysMsgf( ActorPoolActor != nullptr, TEXT( "Actor Pool Actor is not valid!" ) );
 }
 
 #if !( UE_BUILD_SHIPPING || UE_BUILD_TEST )
 void UActorPoolSubSystem::DestroyUnusedInstancesInPools()
 {
-    for ( auto & key_pair : ActorPools )
+    if ( !ensureMsgf( ActorPoolActor != nullptr, TEXT( "%s - ActorPoolActor is not valid!" ), TEXT( __FUNCTION__ ) ) )
     {
-        key_pair.Value.DestroyUnusedInstances();
+        return;
     }
+
+    ActorPoolActor->DestroyUnusedInstancesInPools();
 }
 
 void UActorPoolSubSystem::DumpPoolInfos( FOutputDevice & output_device ) const
 {
-    output_device.Logf( ELogVerbosity::Verbose, TEXT( "Dumping Actor Pool Infos :" ) );
-
-    for ( auto & key_pair : ActorPools )
-    {
-        key_pair.Value.DumpPoolInfos( output_device );
-    }
-}
-#endif
-
-void UActorPoolSubSystem::OnWorldBeginPlay()
-{
-#if !( UE_BUILD_SHIPPING || UE_BUILD_TEST )
-    if ( GActorPoolDisable.GetValueOnGameThread() == 1 )
+    if ( !ensureMsgf( ActorPoolActor != nullptr, TEXT( "%s - ActorPoolActor is not valid!" ), TEXT( __FUNCTION__ ) ) )
     {
         return;
     }
+
+    ActorPoolActor->DumpPoolInfos( output_device );
+}
 #endif
-
-    if ( auto * settings = GetDefault< UActorPoolSettings >() )
-    {
-        for ( const auto & pool_infos : settings->PoolInfos )
-        {
-            ActorPools.Emplace( pool_infos.ActorClass.LoadSynchronous(), CreateActorPoolInstance( pool_infos ) );
-        }
-    }
-}
-
-FActorPoolInstances UActorPoolSubSystem::CreateActorPoolInstance( const FActorPoolInfos & pool_infos ) const
-{
-    return FActorPoolInstances( GetWorld(), pool_infos );
-}
